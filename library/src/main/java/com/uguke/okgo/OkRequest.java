1783b22e4552fa1c8d109b0d12ff18a422393030
LeiJue <tapmail@qq.com>
2018-12-19 21:03:51 +0800

    commit

--- library/src/main/java/com/uguke/okgo/OkRequest.java
+++ library/src/main/java/com/uguke/okgo/OkRequest.java
@@ -2,7 +2,6 @@ package com.uguke.okgo;
 
 import android.content.Context;
 import android.support.annotation.ColorInt;
-import android.support.annotation.StringRes;
 import android.support.v4.content.ContextCompat;
 import android.text.TextUtils;
 
